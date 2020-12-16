a0b15452a98be1e6b8e30f4aa58ec002245143b8
BlueBearrii <thananan.math.acs@gmail.com>
2020-12-16 15:41:26 +0700

    :zap: Initial state and set state from API to response on display

--- client/lib/components/score.dart
+++ client/lib/components/score.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class Score extends StatelessWidget {
   @override
@@ -13,9 +15,48 @@ class Score extends StatelessWidget {
   }
 }
 
-class ScoreScreen extends StatelessWidget {
+class ScoreScreen extends StatefulWidget {
+  @override
+  _ScoreScreenState createState() => _ScoreScreenState();
+}
+
+class _ScoreScreenState extends State<ScoreScreen> {
+  @override
+  int _confirmed = 0;
+  int _deaths = 0;
+  int _hospitalized = 0;
+
+  Future<List> callAPI() async {
+    var apiThai = 'https://covid19.th-stat.com/api/open/today';
+    await http.get(apiThai).then((value) {
+      var getState = convert.jsonDecode(value.body);
+      _confirmed = getState["Confirmed"];
+      _deaths = getState["Deaths"];
+      _hospitalized = getState["Hospitalized"];
+
+      print(getState['Confirmed'].runtimeType);
+    });
+    var list = [_confirmed, _deaths, _hospitalized];
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("test"));
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          callAPI();
+        },
+      ),
+      body: FutureBuilder(
+          future: callAPI(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done)
+              return Container(
+                  child: Center(child: Text(snapshot.data[0].toString())));
+            else if (snapshot.connectionState == ConnectionState.waiting)
+              return Center(child: CircularProgressIndicator());
+          }),
+    );
   }
 }
