87f28e1e273c2a77ed99f1424ffc631f7858e8e1
BlueBearrii <thananan.math.acs@gmail.com>
2020-12-18 16:38:40 +0700

    :zap::wrench: Logout navigation and routes adjusting

--- client/lib/components/app/mainScreen.dart
+++ client/lib/components/app/mainScreen.dart
@@ -1,5 +1,6 @@
 import 'package:client/components/app/contactScreen.dart';
 import 'package:client/components/app/worldCovid.dart';
+import 'package:client/components/login/login.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -16,9 +17,10 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        resizeToAvoidBottomPadding: false,
         appBar: AppBar(
           backgroundColor: Color(0xFF3B3D58),
-          title: currentIndex == 0 ? Text("Covid") : Text("Contact"),
+          title: currentIndex == 0 ? Text("Covid score") : Text("Contact us"),
           actions: [currentIndex == 1 ? LogoutIcon() : Container()],
         ),
         body: currentIndex == 0 ? WorldScore() : ContactScreen(),
@@ -56,7 +58,24 @@ class LogoutIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        print("Logged out");
+        return Navigator.pushAndRemoveUntil(
+            context,
+            PageRouteBuilder(pageBuilder: (BuildContext context,
+                Animation animation, Animation secondaryAnimation) {
+              return Login();
+            }, transitionsBuilder: (BuildContext context,
+                Animation<double> animation,
+                Animation<double> secondaryAnimation,
+                Widget child) {
+              return new SlideTransition(
+                position: new Tween<Offset>(
+                  begin: const Offset(1.0, 0.0),
+                  end: Offset.zero,
+                ).animate(animation),
+                child: child,
+              );
+            }),
+            (route) => false);
       },
       child: Container(child: Icon(Icons.logout)),
     );
